703d57a9465e97e6294a6ed84e0c5a4970456601
leeyh <leeyh@isung.com>
2019-12-27 03:56:14 +0900

    10번문제 해결

--- Question_10/Python.py
+++ Question_10/Python.py
@@ -5,6 +5,8 @@ import re
 
 # 2H 3D 5S 9C KD 2C 3H 4S 8C AH
 # 2H 4S 4C 2D 4H 2S 8S AS QS 3C
+# 2H 3D 5S 9C KD 2C 3H 4S 8C KH
+# 2H 3D 5S 9C KD 2D 3H 5C 9S KH
 
 class RuleResult(enum.Enum):
     HIGH_CARD = 0
@@ -52,39 +54,54 @@ class Rule :
         else :
             return True
     
-    def rule_Check(self) :
-        
-        if self.rule_One_Pair() == RuleResult.ONE_PAIR :
-            return
-        elif self.rule_Two_Pair() == RuleResult.TWO_PAIR :
-            return
-        elif self.rule_Three_Card() == RuleResult.THREE_CARD :
-            return
-        elif self.rule_Straight() == RuleResult.STRAIGHT :
-            return
-        elif self.rule_Flush() == RuleResult.FLUSH :
-            return
-        elif self.rule_Full_House() == RuleResult.FULL_HOUSE :
-            return
-        elif self.rule_FourCard() == RuleResult.FOUR_CARD :
-            return
-        elif self.rule_Straight_Flush() == RuleResult.STRAIGHT_FLUSH :
-            return
-        elif self.rule_High_Card() == RuleResult.HIGH_CARD :
-            return
+    def rule_Check(self, arg) :
+        if self.rule_Straight_Flush(arg) == RuleResult.STRAIGHT_FLUSH :
+            return 8
+        elif self.rule_FourCard(arg) == RuleResult.FOUR_CARD :
+            return 7
+        elif self.rule_Full_House(arg) == RuleResult.FULL_HOUSE :
+            return 6
+        elif self.rule_Flush(arg) == RuleResult.FLUSH :
+            return 5
+        elif self.rule_Straight(arg) == RuleResult.STRAIGHT :
+            return 4
+        elif self.rule_Three_Card(arg) == RuleResult.THREE_CARD :
+            return 3
+        elif self.rule_Two_Pair(arg) == RuleResult.TWO_PAIR :
+            return 2
+        elif self.rule_One_Pair(arg) == RuleResult.ONE_PAIR :
+            return 1
+        elif self.rule_High_Card(arg) == RuleResult.HIGH_CARD :
+            return 0
         else :
-            print('rule_Check None')
+            return -1
 
-    def rule_High_Card(self) :
+    def cardConvertInt(self, arg) :
+        if arg == 'A' :
+            return 14
+        elif arg == 'K' :
+            return 13
+        elif arg == 'Q' :
+            return 12
+        elif arg == 'J' :
+            return 11
+        elif arg == 'T' :
+            return 10
+        else :
+            return 0
+
+    def rule_High_Card(self, arg) :
         print('rule_High_Card start')
         if self.checkMeEnemyCount() :
-            for i in range(0, 5) :
-                for j in range(0, 5) :
-                    if self.me[i] > self.enemy[j] :
-                        return RuleResult.HIGH_CARD
-                    elif self.me[i] < self.enemy[j] :
-                        return RuleResult.HIGH_CARD
-            return RuleResult.NONE
+            index = 0
+            for i in arg :
+                if i.number.isdigit() and index == 0 :
+                    index = int(i.number)
+                elif i.number.isdigit() and index+1 == int(i.number) :
+                    index = int(i.number)
+                else :
+                    return RuleResult.NONE
+            return RuleResult.HIGH_CARD
         else :
             return RuleResult.ERROR
 
@@ -94,247 +111,158 @@ class Rule :
         seen_twice = set( x for x in seq if x in seen or seen_add(x) )
         return list(seen_twice)
 
-    def rule_One_Pair(self) :
+    def rule_One_Pair(self, arg) :
         print('rule_One_Pair start')
         if self.checkMeEnemyCount() :
-            me_same_card = self.list_duplicates(self.me)
-            enemy_same_card = self.list_duplicates(self.enemy)
-            ms_count = len(me_same_card)
-            es_count = len(enemy_same_card)
-            print('ms_count >>>>>>>>>>>>>>>>> '+str(ms_count))
-            print('es_count >>>>>>>>>>>>>>>>> '+str(es_count))
-            if ms_count == 1 or es_count == 1 :
-                if ms_count == 1 :
-                    return RuleResult.ONE_PAIR
-                elif es_count == 1 :
-                    return RuleResult.ONE_PAIR
-                else :
-                    return RuleResult.NONE
-            elif ms_count == 1 and es_count == 1 :
-                if me_same_card[0] > me_same_card[0] :
-                        return RuleResult.ONE_PAIR
-                elif me_same_card[0] < me_same_card[0] :
-                    return RuleResult.ONE_PAIR
-                else :
-                    me_same_card = list(set(self.me) - set(self.me_same_card))
-                    enemy_same_card = list(set(self.enemy) - set(self.enemy_same_card))
-                    if len(me_same_card) > 1 and len(enemy_same_card) > 1 :
-                        for i in range(0, 3) :
-                            for j in range(0, 3) :
-                                if me_same_card[i] > me_same_card[j] :
-                                    return RuleResult.TWO_PAIR
-                                elif me_same_card[i] < me_same_card[j] :
-                                    return RuleResult.TWO_PAIR               
+            same_card = self.list_duplicates(arg)
+            count = len(same_card)
+            if count == 1 :
+                return RuleResult.ONE_PAIR
             else :
                 return RuleResult.NONE
         else :
             return RuleResult.ERROR
 
-    def rule_Two_Pair(self) :
+    def rule_Two_Pair(self, arg) :
         print('rule_Two_Pair start')
         if self.checkMeEnemyCount() :
-            me_same_card = self.list_duplicates(self.me)
-            enemy_same_card = self.list_duplicates(self.enemy)
-            ms_count = len(me_same_card)
-            es_count = len(enemy_same_card)
-            print('ms_count >>>>>>>>>>>>>>>>> '+str(ms_count))
-            print('es_count >>>>>>>>>>>>>>>>> '+str(es_count))
-            if  ms_count == 2 or es_count == 2 :
-                if ms_count == 2 :
-                    return RuleResult.ONE_PAIR
-                elif es_count == 2 :
-                    return RuleResult.ONE_PAIR
-                else :
-                    return RuleResult.NONE
-            elif ms_count == 2 and es_count == 2 :
-                me_same_card.sort(reverse=True)
-                enemy_same_card.sort(reverse=True)
-                if me_same_card[0] > me_same_card[0] :
-                    return RuleResult.ONE_PAIR
-                elif me_same_card[0] < me_same_card[0] :
-                    return RuleResult.ONE_PAIR
-                else :
-                    me_same_card = list(set(self.me) - set(self.me_same_card))
-                    enemy_same_card = list(set(self.enemy) - set(self.enemy_same_card))
-                    for i in range(0, 1) :
-                        for j in range(0, 1) :
-                            if me_same_card[i] > me_same_card[j] :
-                                return RuleResult.TWO_PAIR
-                            elif me_same_card[i] < me_same_card[j] :
-                                return RuleResult.TWO_PAIR
-                    return RuleResult.NONE
+            same_card = self.list_duplicates(arg)
+            count = len(same_card)
+            if count == 2 :
+                return RuleResult.TWO_PAIR
             else :
                 return RuleResult.NONE
         else :
             return RuleResult.ERROR
 
-    def sub_Check_Three_Same_Card(self, arg) :
-        print('')
-
-    def rule_Three_Card(self) :
+    def rule_Three_Card(self, arg) :
         print('rule_Three_Card start')
         if self.checkMeEnemyCount() :
-            me_same_card = self.list_duplicates(self.me)
-            enemy_same_card = self.list_duplicates(self.enemy)
-            ms_count = len(me_same_card)
-            es_count = len(enemy_same_card)
-            if ms_count > 0 and ms_count <= 2 or es_count > 0 and es_count <= 2:
-                if ms_count > 0 and ms_count <= 2 :
+            same_card = self.list_duplicates(arg)
+            for i in same_card :
+                if self.countX(arg, i.number) == 3 :
                     return RuleResult.THREE_CARD
-                elif es_count > 0 and es_count <= 2 :
-                    return RuleResult.THREE_CARD
-                else :
-                    return RuleResult.NONE
-            elif ms_count > 0 and ms_count <= 2 and es_count > 0 and es_count <= 2:
-                print('ms_count >>>>>>>>>>>>>>>>> '+str(ms_count))
-                print('es_count >>>>>>>>>>>>>>>>> '+str(es_count))
-                ms_count = self.sub_Check_Three_Same_Card(me_same_card)
-                es_count = self.sub_Check_Three_Same_Card(enemy_same_card)
-                if ms_count == 3 and es_count == 3 :
-                    me_same_card.sort(reverse=True)
-                    enemy_same_card.sort(reverse=True)
-                    if me_same_card[0] > me_same_card[0] :
-                        return RuleResult.ONE_PAIR
-                    elif me_same_card[0] < me_same_card[0] :
-                        return RuleResult.ONE_PAIR
-                    else :
-                        me_same_card = list(set(self.me) - set(self.me_same_card))
-                        enemy_same_card = list(set(self.enemy) - set(self.enemy_same_card))
-                        for i in range(0, 3) :
-                            for j in range(0, 3) :
-                                if me_same_card[i] > me_same_card[j] :
-                                    return RuleResult.TWO_PAIR
-                                elif me_same_card[i] < me_same_card[j] :
-                                    return RuleResult.TWO_PAIR
-                        return RuleResult.NONE
-                else :
-                    return RuleResult.NONE
+            return RuleResult.NONE
         else :
-            return RuleResult.ERROR
+            return RuleResult.NONE
 
-    def rule_Straight(self) :
+    def countX(self, lst, x): 
+        count = 0
+        for ele in lst: 
+            if (ele.number == x): 
+                count = count + 1
+        return count
+
+    def rule_Straight(self, arg) :
         print('rule_Straight start')
         if self.checkMeEnemyCount() :
             index = 0
-            for i in self.me :
+            for i in arg :
                 if i.number.isdigit() and index == 0 :
                     index = int(i.number)
                 elif i.number.isdigit() and index+1 == int(i.number) :
                     index = int(i.number)
                 else :
                     return RuleResult.NONE
-            index = 0
-            for j in self.enemy :
-                if index == 0 :
-                    index = int(j.number)
-                elif i.number.isdigit() and index+1 == int(j.number) :
-                    index = int(j.number)
-                else :
-                    return RuleResult.NONE
-            for i in range(0, 5) :
-                for j in range(0, 5) :
-                    if self.me[i] > self.enemy[j] :
-                        return RuleResult.STRAIGHT
-                    elif self.me[i] < self.enemy[j] :
-                        return RuleResult.STRAIGHT
             return RuleResult.STRAIGHT
         else :
             return RuleResult.ERROR
 
-    def rule_Flush(self) :
+    def rule_Flush(self, arg) :
         print('rule_Flush start')
         if self.checkMeEnemyCount() :
             index = ""
-            for i in self.me :
+            for i in arg :
                 if index == "" :
                     index = i.type
                 elif index == i.type :
                     index = i.type
                 else :
                     return RuleResult.NONE
-            index = ""
-            for j in self.enemy :
-                if index == "" :
-                    index = i.type
-                elif index == i.type :
-                    index = i.type
-                else :
-                    return RuleResult.NONE
-            for i in range(0, 5) :
-                for j in range(0, 5) :
-                    if self.me[i] > self.enemy[j] :
-                        return RuleResult.STRAIGHT
-                    elif self.me[i] < self.enemy[j] :
-                        return RuleResult.STRAIGHT
+            return RuleResult.FLUSH
         else :
             return RuleResult.ERROR
 
-    def rule_Full_House(self) :
+    def rule_Full_House(self, arg) :
         print('rule_Full_House start')
         if self.checkMeEnemyCount() :
-            me_same_card = list(set(self.me).intersection(self.me))
-            enemy_same_card = list(set(self.enemy).intersection(self.enemy))
-            if me_same_card.count == 3 and enemy_same_card.count == 3 :
-                me_same_card.sort(reverse=True)
-                enemy_same_card.sort(reverse=True)
-                for i in range(0, 3) :
-                    for j in range(0, 3) :
-                        if me_same_card[i] > me_same_card[j] :
-                            return RuleResult.THREE_CARD
-                        elif me_same_card[i] < me_same_card[j] :
-                            return RuleResult.THREE_CARD
-            else :
+            same_card = self.list_duplicates(arg)
+            if len(same_card) == 2 : 
+                i = self.countX(arg, same_card[0].number)
+                j = self.countX(arg, same_card[1].number)
+                print('rule_Full_House i : '+str(i))
+                print('rule_Full_House i : '+str(j))
+                if (i+j) == 5 : 
+                    return RuleResult.FULL_HOUSE
+                else : 
+                    return RuleResult.NONE
+            else : 
                 return RuleResult.NONE
         else :
             return RuleResult.ERROR
 
-    def rule_FourCard(self) :
+    def rule_FourCard(self, arg) :
         print('rule_FourCard start')
         if self.checkMeEnemyCount() :
-            me_same_card = list(set(self.me).intersection(self.me))
-            enemy_same_card = list(set(self.enemy).intersection(self.enemy))
-            me_count = len(set(me_same_card) - set(self.me))
-            enemy_count = len(set(enemy_same_card) - set(self.enemy))
-            if me_count == 4 and enemy_count == 4 :
-                me_same_card.sort(reverse=True)
-                enemy_same_card.sort(reverse=True)
-                if me_same_card[0] > me_same_card[0] :
-                    return RuleResult.ONE_PAIR
-                elif me_same_card[0] < me_same_card[0] :
-                    return RuleResult.ONE_PAIR
-                else :
-                    me_same_card = list(set(self.me) - set(self.me_same_card))
-                    enemy_same_card = list(set(self.enemy) - set(self.enemy_same_card))
-                    for i in range(0, 3) :
-                        for j in range(0, 3) :
-                            if me_same_card[i] > me_same_card[j] :
-                                return RuleResult.FOUR_CARD
-                            elif me_same_card[i] < me_same_card[j] :
-                                return RuleResult.FOUR_CARD
-                    return RuleResult.NONE
-            else :
-                return RuleResult.NONE
+            same_card = self.list_duplicates(arg)
+            for i in same_card :
+                if self.countX(arg, i.number) == 4 :
+                    return RuleResult.FOUR_CARD
+            return RuleResult.NONE
         else :
-            return RuleResult.ERROR
+            return RuleResult.NONE
 
-    def rule_Straight_Flush(self) :
+    def rule_Straight_Flush(self, arg) :
         print('rule_Straight_Flush start')
         if self.checkMeEnemyCount() :
-            if self.rule_Flush() == RuleResult.FLUSH and self.rule_Straight() == RuleResult.STRAIGHT :
-                for i in range(0, 5) :
-                    for j in range(0, 5) :
-                        if self.me[i] > self.enemy[j] :
-                            return RuleResult.STRAIGHT_FLUSH
-                        elif self.me[i] < self.enemy[j] :
-                            return RuleResult.STRAIGHT_FLUSH
-                return RuleResult.NONE
-            else :
-                return RuleResult.NONE
+            index1 = ""
+            for i in arg :
+                if index1 == "" :
+                    index1 = i.type
+                elif index1 == i.type :
+                    index1 = i.type
+                else :
+                    return RuleResult.NONE
+            index2 = 0
+            for i in arg :
+                if i.number.isdigit() and index2 == 0 :
+                    index = int(i.number)
+                elif i.number.isdigit() and index2+1 == int(i.number) :
+                    index2 = int(i.number)
+                else :
+                    return RuleResult.NONE
+            return RuleResult.STRAIGHT_FLUSH
         else :
             return RuleResult.ERROR
 
     def getResult(self) :
-        print('')
+        me_result = self.rule_Check(self.me)
+        print('me_result >>>>>>>>> '+str(me_result))
+        enemy_result = self.rule_Check(self.enemy)
+        print('enemy_result >>>>>>>>> '+str(enemy_result))
+        if me_result > enemy_result :
+            print('Black Win')
+        elif me_result < enemy_result :
+            print('White Win')
+        else : 
+            for i in range(0, 5) :
+                if not self.me[i].number.isdigit() :
+                    me = self.cardConvertInt(self.me[i].number)
+                else :
+                    me = int(self.me[i].number)
+                if not self.enemy[i].number.isdigit() :
+                    enemy = self.cardConvertInt(self.enemy[i].number)
+                else :
+                    enemy = int(self.enemy[i].number)
+                if me > enemy :
+                    print('Black Win')
+                    return
+                elif me < enemy :
+                    print('White Win')
+                    return
+            print('Tie')
+
+                
 
 class Card(object) :
     def __init__(self, name) :
@@ -389,7 +317,5 @@ for commands in inputArray :
 rule.serperateMeEnemyCard()
 
 rule.sortCards()
-
-rule.rule_Check()
 
 rule.getResult()
--- Question_10/README.md
+++ Question_10/README.md
@@ -1,1 +1,7 @@
-파이썬으로 작성되었습니다.
+파이썬으로 작성되었습니다.<br>
+*이 문제의 단점<br>
+ - 카드의 룰에 대한 설명이 너무 부족하다.<br>
+ - 나와 적 사이에 가지고 있는 카드들에 대한 각각 파악이 필요<br>
+    예) 나는 One-Pair 적은 Two-Pair인경우? (높고 or 낮음)<br>
+*높고 낮음<br>
+    HIGH_CARD < ONE_PAIR < TWO_PAIR < THREE_CARD < STRAIGHT < FLUSH < FULL_HOUSE < FOUR_CARD < STRAIGHT_FLUSH
